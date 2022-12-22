7efe46af2f079318f179f6afc979cd35a80ca9e2
Nicolas Ortega <11nicolasortega@gmail.com>
2022-12-22 13:40:08 -0500

    update header

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a test to see if it updates in real time.
+        </p>
+        <p>
+          ^Nicolas made this edit.
         </p>
         <a
           className="App-link"
